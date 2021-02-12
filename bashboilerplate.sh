16c36e196cd3ee5cdf91f8b50fed0b4db1b30974
Kurt Duyck <kurt.duyck@protonmail.com>
2021-02-12 23:39:51 +0100

    Tuning bash boilerplate
    	modified:   CHANGELOG.md
    	modified:   bashboilerplate.sh

--- bashboilerplate.sh
+++ bashboilerplate.sh
@@ -1,33 +1,41 @@
 #!/usr/bin/env bash
 set -euo pipefail
-IFS=$'\n\t'
-# set -e immediately halts the script when unhandled error appears.
-# set -u affects variables. When set, a reference to any variable you haven't previously defined - with the exceptions of $* and $@ - is an error, and causes the program to immediately exit. 
-# set -o pipefail
-# This setting prevents errors in a pipeline from being masked.
-# The IFS variable - which stands for Internal Field Separator - controls what Bash calls word splitting.
+IFS=$'\n\t' # The IFS variable - which stands for Internal Field Separator 
 
+usage() {
+  cat <<EOF
+Usage: ./$(basename "$0") [-h] [-V] 
+Bash kitchen sink
 
-if [[ ${1+x} ]]; then 
- 
-  if [ "$1" == "-h"  ] || [ "$1" == "--help" ]; then
-    echo "Usage: ./$(basename "$0")"
-    echo "Usage: $(basename "$0") --help"
-    echo "Usage: $(basename "$0") --version"
-    exit 0
-  fi
-  
-  # --version
-  if [ "$1" == "-V"  ] || [ "$1" == "--version" ]; then
-    echo "v0.1.0"
-    exit 0
-  fi
+Options:
 
-else 
+-h, --help      Print this help and exit
+-V, --version   Print version
+EOF
+  exit
+}
 
-  echo "-------------------------------"
-  echo "       bash kitchen "
-  echo "-------------------------------"
+version() {
+  cat <<EOF
+v0.1.1
+EOF
+  exit 0
+}
 
-fi
-# EOF
+parse_params() {
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -V | --version) version ;;
+    *) break ;;
+    esac
+    shift
+  done
+  return 0
+}
+
+parse_params "$@"
+
+echo "-------------------------------"
+echo "       bash kitchen "
+echo "-------------------------------"
