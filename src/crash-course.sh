086e364ec84adf033bd90c7618210429ff3c845a
Kurt Duyck <kurt.duyck@protonmail.com>
2021-04-02 12:50:23 +0200

    Loop 1..10
    	modified:   CHANGELOG.md
    	modified:   src/crash-course.sh

--- src/crash-course.sh
+++ src/crash-course.sh
@@ -68,3 +68,34 @@ else
 fi
 
 
+# print numbers 1 to 10
+
+# c like for loop
+for (( i = 1; i <= 10; ++i ))
+do
+  echo "$i"
+done
+
+# for in
+for i in {1..10}
+do
+  echo "$i"
+done
+
+# while
+i=1
+while [[ "$i" -le 10 ]]
+do
+  echo "$i"
+  ((i++))
+done
+
+# until
+i=1
+until [[ "$i" -eq 11 ]]
+do
+  echo "unitl $i"
+  ((i++))
+done
+
+
